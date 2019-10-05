003e4706c5d53bb7b5c955bca8a76c01a77bf89f
Andres Alvarez <andresalvarez353@gmail.com>
2019-10-05 22:09:07 +0100

    Implement Pygram as a context manager

--- pygram/pygram.py
+++ pygram/pygram.py
@@ -5,19 +5,27 @@ from .workspace import UserWorkspace
 
 
 class Pygram:
+    """
+    Initializes a Pygram session.
+    """
+
     def __init__(self, username: str, password: str):
         self.username = username
         self.password = password
 
+    def __enter__(self):
         self.workspace = UserWorkspace(self.username)
         self._create_user_workspace()
 
         self.browser = PygramBrowserFactory.firefox_browser()
-
         self.browser.implicitly_wait(5)
-        self.browser.get(INSTAGRAM_HOMEPAGE_URL)
 
         self._login()
+
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.browser.quit()
 
     def _create_user_workspace(self):
         if not self.workspace.exists:
@@ -27,5 +35,8 @@ class Pygram:
         """
         Login the user using the crendetials provided.
         """
+
+        self.browser.get(INSTAGRAM_HOMEPAGE_URL)
+
         authenticator = Authenticator(self.username, self.password, self.browser)
         authenticator.login()
