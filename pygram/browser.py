770289bfe3d1fa8629a41c72fa8eb146f8eba191
Andres Alvarez <andresalvarez353@gmail.com>
2019-10-11 16:29:01 -0600

    Use browser instance as a global lazy browser

--- pygram/browser.py
+++ pygram/browser.py
@@ -6,10 +6,18 @@ FIREFOX_USER_AGENT = (
     'Mozilla/5.0 (iPhone; CPU iPhone OS 12_1 like Mac OS X) AppleWebKit/605.1.15 '
     '(KHTML, like Gecko) FxiOS/18.1 Mobile/16B92 Safari/605.1.15'
 )
+_CURRENT_BROWSER = None
+
+
+def get_browser():
+    global _CURRENT_BROWSER
+    if _CURRENT_BROWSER is None:
+        _CURRENT_BROWSER = PygramBrowserFactory.firefox_browser()
+
+    return _CURRENT_BROWSER
 
 
 class PygramBrowserFactory:
-
     @staticmethod
     def firefox_browser():
         """
@@ -20,9 +28,7 @@ class PygramBrowserFactory:
 
         # Set English language
         firefox_profile.set_preference('intl.accept_languages', 'en-US')
-        firefox_profile.set_preference(
-            'general.useragent.override', FIREFOX_USER_AGENT
-        )
+        firefox_profile.set_preference('general.useragent.override', FIREFOX_USER_AGENT)
 
         # mute audio while watching stories
         firefox_profile.set_preference('media.volume_scale', '0.0')
