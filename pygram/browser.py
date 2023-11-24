5778d45e669ef8bc29a84721d4db7ac724fe9635
Andrés Álvarez <andresalvarez353@gmail.com>
2023-11-24 11:41:17 -0600

    Update Selenium Firefox browser

--- pygram/browser.py
+++ pygram/browser.py
@@ -1,47 +1,31 @@
-from selenium.webdriver.firefox.webdriver import WebDriver
-from selenium.webdriver.firefox.webdriver import FirefoxProfile
+from selenium import webdriver
 
 
 FIREFOX_USER_AGENT = (
-    'Mozilla/5.0 (iPhone; CPU iPhone OS 12_1 like Mac OS X) AppleWebKit/605.1.15 '
-    '(KHTML, like Gecko) FxiOS/18.1 Mobile/16B92 Safari/605.1.15'
+    "Mozilla/5.0 (iPhone; CPU iPhone OS 12_1 like Mac OS X) AppleWebKit/605.1.15 "
+    "(KHTML, like Gecko) FxiOS/18.1 Mobile/16B92 Safari/605.1.15"
 )
+
 _CURRENT_BROWSER = None
+DEFAULT_WINDOW_WIDTH = 375
+DEFAULT_WINDOW_HEIGHT = 812
 
 
 def get_browser():
     global _CURRENT_BROWSER
+
     if _CURRENT_BROWSER is None:
-        _CURRENT_BROWSER = PygramFirefoxBrowser()
-
-    return _CURRENT_BROWSER
-
-
-class PygramFirefoxBrowser(WebDriver):
-    """
-    Selenium FireFox web driver with special settings.
-    """
-
-    def __init__(self, *args, **kwargs):
-        firefox_profile = FirefoxProfile()
+        options = webdriver.FirefoxOptions()
 
         # Set English language
-        firefox_profile.set_preference('intl.accept_languages', 'en-US')
-        firefox_profile.set_preference('general.useragent.override', FIREFOX_USER_AGENT)
+        options.set_preference("intl.accept_languages", "en-US")
+        options.set_preference("general.useragent.override", FIREFOX_USER_AGENT)
 
         # mute audio while watching stories
-        firefox_profile.set_preference('media.volume_scale', '0.0')
+        options.set_preference("media.volume_scale", "0.0")
 
-        super().__init__(firefox_profile=firefox_profile, *args, **kwargs)
+        _CURRENT_BROWSER = webdriver.Firefox(options=options)
+        _CURRENT_BROWSER.set_window_size(DEFAULT_WINDOW_WIDTH, DEFAULT_WINDOW_HEIGHT)
+        return _CURRENT_BROWSER
 
-        # Set mobile viewport (iPhone X)
-        self.set_window_size(375, 812)
-
-    def navigate(self, url: str):
-        """Navigates to a URL if the current page is not on that URL."""
-
-        assert 'instagram.com' in url
-
-        # NOTE: `url` must include the www subdomain
-        if url not in self.current_url:
-            self.get(url)
+    return _CURRENT_BROWSER
