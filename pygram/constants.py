0f40121b1fd31e8fb0775e2ac4a592a9067d9019
Andrés Álvarez <andresalvarez353@gmail.com>
2023-11-24 19:42:15 -0600

    Remove unused constants

--- pygram/constants.py
+++ pygram/constants.py
@@ -3,10 +3,6 @@ from enum import Enum
 
 INSTAGRAM_HOMEPAGE_URL = "https://www.instagram.com"
 
-MEDIA_PHOTO = "photo"
-MEDIA_VIDEO = "video"
-MEDIA_CAROUSEL_ = "carousel"
-
 
 class FollowingStatus(str, Enum):
     FOLLOWING = "Following"
